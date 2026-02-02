94782fb8f5fa5cd2ee8f9c3bf2348ad666d11831
Iranzi Response <iranzi.response@students.mak.ac.ug>
2026-02-02 15:13:58 +0200

    add number variables and display them

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main{
     public static void main(String[] args){
         System.out.println("==Simple Calculator==");
+
+        //calculator variables
+
+         double num1 = 10;
+         double num2 = 5;
+        System.out.println("value on num1: " + num1);
+        System.out.println("value on num2: " + num2);
+
     }
 }
