95000533dfa4d4de7a256dceca0fadcec3e219bc
Iranzi Response <iranzi.response@students.mak.ac.ug>
2026-02-02 16:26:25 +0200

    Add input functionality with scanner

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         System.out.println("==Simple Calculator==");
 
+        //create a scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        //enter the first number
+        System.out.println("Enter your first number: ");
+        double num1 = scanner.nextInt();
+
+        //enter your second number
+        System.out.println("Enter your next number: ");
+        double num2 = scanner.nextInt();
+
+        System.out.println("You entered: "+ num1 + "and"+ num2);
+
         //calculator object
         Calculator calc = new Calculator();
-
-        // calculator variables
-        double num1 = 10;
-        double num2 = 5;
-        System.out.println("value on num1: " + num1);
-        System.out.println("value on num2: " + num2);
         System.out.println();
 
 
@@ -28,6 +37,8 @@ public class Main{
         //Division
         double division = calc.division(num1,num2) ;
         System.out.println("Multiplication: " + num1 + "/" + num2 + "=" + division);
+
+        scanner.close();
 
 
 
