36bea62bff89d8afb9fba65d55ddb7cfc54925a4
Iranzi Response <iranzi.response@students.mak.ac.ug>
2026-02-02 15:37:59 +0200

    All the variable declarations

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,25 @@ public class Main{
         System.out.println("value on num1: " + num1);
         System.out.println("value on num2: " + num2);
 
+        //addition
+        double sum = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2  +" = " + sum);
+
+        //Subtraction
+        double difference = num1 - num2;
+        System.out.println("Subtraction: " + num1 + "-" + num2 + "=" + difference);
+
+        //Multiplication
+        double product = num1 * num2;
+        System.out.println("Multiplication: " + num1 + "*" + num2 + "=" + product);
+
+        //Division
+        double division = num1 / num2;
+        System.out.println("Multiplication: " + num1 + "/" + num2 + "=" + division);
+
+
+
+
+
     }
 }
